d51f1e53189754fc2fd3c111d1fb441a71a2f60c
Shubham Mandal <shubhammandal96@gmail.com>
2020-08-24 17:47:28 +0530

    kongGame_2D_tut3

---
+++ runner_2D/Assets/GAME/script/spawnWallandBirdScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnWallandBirdScript : MonoBehaviour
+{
+    private Queue<GameObject> wallObj_queue = new Queue<GameObject>();
+    private long playerScore = 0;
+    public GameObject wallObjForSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (playerScore % 200 == 0)
+        {
+            GameObject Instant_wallObj = Instantiate( wallObjForSpawn );
+            wallObj_queue.Enqueue(Instant_wallObj);
+        }
+
+        foreach(GameObject g in wallObj_queue)
+        {
+            if( g.transform.position.x <= -10)
+            {
+                Destroy(g);
+                wallObj_queue.Dequeue();
+            }
+            Debug.Log(g.name);
+        }
+
+
+        playerScore++;
+        // Debug.Log("Player Score :- "+ playerScore);
+    }
+}
---
+++ runner_2D/Assets/GAME/script/wallObjScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallObjScript : MonoBehaviour
+{
+    public float wallVelocity = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if ( this.transform.position.x > -10 )
+        {
+            this.transform.position -= new Vector3(wallVelocity, 0, 0);
+        }
+        
+    }
+}

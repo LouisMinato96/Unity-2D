9bad9912d287d23ace1a8ce7a257be6bacdf3eed
Shubham Mandal <shubhammandal96@gmail.com>
2020-08-24 17:54:34 +0530

    runner_2D_tut

---
+++ runner_2D_tut/Assets/mainSceneGameControllerScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class mainSceneGameControllerScript : MonoBehaviour
+{
+    public playerJump player;
+    public Camera cam;
+    public Text gameScoreText;
+
+    public GameObject[] blockPrefabs;
+
+    private float blockPointer = 0;
+    private float safeSpace = 21;
+    private bool isGameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blockPointer = -12;
+    }
+
+    void generateNewBlock()
+    {
+        int blockIndex = Random.Range(1, blockPrefabs.Length);
+        if(blockPointer < 0)
+        {
+            blockIndex = 0;
+        }
+
+        GameObject blockObj = Instantiate(blockPrefabs[blockIndex]);
+        blockObj.transform.SetParent(this.transform);
+        Scr_block block = blockObj.GetComponent<Scr_block>();
+
+        blockObj.transform.position = new Vector3(
+            blockPointer + block.size/2,
+            0,
+            0
+            );
+        blockPointer += (block.size);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            cam.transform.position = new Vector3(
+                player.transform.position.x,
+                0,
+                -10
+            );
+
+            gameScoreText.text = "Score : " + Mathf.Floor(player.transform.position.x);
+
+            if (player.transform.position.x > blockPointer - safeSpace)
+            {
+                generateNewBlock();
+            }
+        }
+        else
+        {
+            if (!isGameOver)
+            {
+                isGameOver = true;
+                gameScoreText.text += "\n GAME OVER \nPress R to Restart";
+            }
+
+            if ( Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
+    }
+}
---
+++ runner_2D_tut/Assets/playerJump.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerJump : MonoBehaviour
+{   
+    // Public Variable
+    public long jumpForce;
+    public int playerSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Input.GetKeyDown(KeyCode.Space))
+        {
+            this.GetComponent<Rigidbody2D>().AddForce( new Vector2(0,jumpForce) );
+        }
+
+        this.GetComponent<Rigidbody2D>().velocity = new Vector2(
+                playerSpeed,
+                this.GetComponent<Rigidbody2D>().velocity.y
+            );
+    }
+
+    private void OnTriggerEnter2D(Collider2D c)
+    {
+        Debug.Log( c.tag);
+        if (c.tag == "obstacle")
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
